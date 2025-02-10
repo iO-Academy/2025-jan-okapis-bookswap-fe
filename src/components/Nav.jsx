f3a0921ef003ffd4a3aeed7868707748b201d0d7
Sammy Bhaidani <sammybhaidani@gmail.com>
2025-02-10 16:58:23 +0000

    Add nav to app

---
+++ src/components/Nav.jsx
@@ -0,0 +1,9 @@
+import H1 from "./atoms/H1";
+
+export default function Nav() {
+  return (
+    <nav className="bg-yellow-500 p-4">
+      <H1 text="Book Swap Okapi Edition" />
+    </nav>
+  );
+}
