5684f20bed7273ff479be0caddfa7354a116f709
Sammy Bhaidani <sammybhaidani@gmail.com>
2025-02-13 23:43:53 +0000

    Update genreId to be used in home based on selected option

--- src/components/GenreFilter.jsx
+++ src/components/GenreFilter.jsx
@@ -1,12 +1,14 @@
-export default function GenreFilter ( {genres, onFilterChange}) {
+export default function GenreFilter ( {genres, getGenreId}) {
+
     return (
         <div className="flex flex-col items-center text-center mx-auto  gap-2 pt-2 p-5 px-10 md:flex-row md:text-left md:p-5 md:items-start">
 
             <label htmlFor="genre">Genre:</label>
-            <select id="genre" aria-label="Select genre" className="border p-0.5 rounded w-45 md:w-50 lg:w-55 text-base" defaultValue="">
+            <select id="genre" aria-label="Select genre" className="border p-0.5 rounded w-45 md:w-50 lg:w-55 text-base" defaultValue="" onChange={(e) => getGenreId(e.target.value)}>
 
+            <option value="">No genre selected</option>
             {genres.map(genre => 
-                <option key={genre.id} value={genre.name}>{genre.name}</option>
+                <option key={genre.id} value={genre.id}>{genre.name}</option>
             )}
 
             </select>
