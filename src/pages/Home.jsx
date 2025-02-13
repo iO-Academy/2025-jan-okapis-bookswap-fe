1992f3176eebd7838de8dbbf36c7a03d6d58e68f
Sammy Bhaidani <sammybhaidani@gmail.com>
2025-02-13 23:59:11 +0000

    Fix spacing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,19 +4,21 @@ import { Link } from "react-router-dom";
 import GenreFilter from "../components/GenreFilter";
 
 export default function Home({claimed}) {
+
     const [bookInfo, setBookInfo] = useState([])
     const [genres, setGenres] = useState([])
     const [selectedGenre, setSelectedGenre] = useState("")
 
     function getData() {
-        const url = `https://book-swap-api.dev.io-academy.uk/api/books?claimed=${claimed}${selectedGenre ? `&genre=${selectedGenre}` : ''}`;
 
-        fetch(url)
-         .then(res => res.json())
-         .then(fetchedInfo => {
-            setBookInfo(fetchedInfo.data);
-         })
-         .catch(error => console.error("Error fetching books", error))
+      const url = `https://book-swap-api.dev.io-academy.uk/api/books?claimed=${claimed}${selectedGenre ? `&genre=${selectedGenre}` : ''}`;
+
+      fetch(url)
+        .then(res => res.json())
+        .then(fetchedInfo => {
+          setBookInfo(fetchedInfo.data);
+        })
+        .catch(error => console.error("Error fetching books", error))
     }
 
     useEffect(getData, [claimed, selectedGenre])
@@ -33,25 +35,24 @@ export default function Home({claimed}) {
       setSelectedGenre(genreId)
     }
 
-  
-    return(
+    return (
+      <div>
         <div>
-          <div>
-            <GenreFilter genres={genres} getGenreId={handleGenreChange} />
-          </div>
-  
-          <section className="grid grid-cols-1 md:grid-cols-3 max-w[800px]">    
-            {bookInfo.map(book => (
-                <Link key={book.id} to={`/book/${book.id}`} >
-                  <SingleBook
-                    title={book.title}
-                    author={book.author}
-                    image={book.image}
-                    genre={book.genre.name}    
-                    />  
-                </Link> )
-            )}
-          </section> 
+          <GenreFilter genres={genres} getGenreId={handleGenreChange} />
         </div>
+
+        <section className="grid grid-cols-1 md:grid-cols-3 max-w[800px]">    
+          {bookInfo.map(book => (
+              <Link key={book.id} to={`/book/${book.id}`} >
+                <SingleBook
+                  title={book.title}
+                  author={book.author}
+                  image={book.image}
+                  genre={book.genre.name}    
+                  />  
+              </Link> )
+          )}
+        </section> 
+      </div>
     )
   }
